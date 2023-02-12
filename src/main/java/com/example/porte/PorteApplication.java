8ecf4ae3a1c0fba8655cd1f9641bf10bab67f0d8
sadekkk12 <sade0027@stud.kea.dk>
2023-02-12 21:00:10 +0100

    Finished

---
+++ src/main/java/com/example/porte/PorteApplication.java
@@ -0,0 +1,13 @@
+package com.example.porte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/porte/PorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.porte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
